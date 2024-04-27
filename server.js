c12dea3e95134bdfba9a24632c9449bb68f48458
ngocchinh17 <chinhwedit@gmail.com>
2024-04-27 14:15:14 +0700

    vercel.js

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-const jsonServer = require("json-server")
-const auth = require("json-server-auth")
-const dayjs = require("dayjs")
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const dayjs = require("dayjs");
 
-const server = jsonServer.create()
-const router = jsonServer.router("./db.json")
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.db = router.db
+server.db = router.db;
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 server.use((req, res, next) => {
   if (req.method === "POST") {
-    req.body.createdAt = dayjs().valueOf()
-    req.body.updatedAt = dayjs().valueOf()
-    req.body.isDelete = false
+    req.body.createdAt = dayjs().valueOf();
+    req.body.updatedAt = dayjs().valueOf();
+    req.body.isDelete = false;
   }
 
   if (req.method === "PUT") {
-    req.method = "PATCH"
+    req.method = "PATCH";
   }
 
   if (req.method === "PATCH") {
-    req.body.updatedAt = dayjs().valueOf()
+    req.body.updatedAt = dayjs().valueOf();
   }
 
-  next()
-})
+  next();
+});
 
-server.use(auth)
-server.use(router)
-server.listen(4000)
+server.use(
+  jsonServer.rewriter({
+    "/*": "/&1",
+  })
+);
+
+server.use(auth);
+server.use(router);
+server.listen(4000);
